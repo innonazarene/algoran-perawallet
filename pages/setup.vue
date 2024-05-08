0b6364afc71f579b59dd2b7611ae84df4f4a0343
Rustom Pedales <pedales.rustom@gmail.com>
2024-05-08 22:00:20 +0800

    Create setup.vue

---
+++ pages/setup.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { PeraWalletConnect } from '@perawallet/connect'
+import { Transaction } from "algosdk"
+const wallet = ref(null)
+const walletOpt = {
+    shouldShowSignTxnToast: false
+}
+wallet.value = new PeraWalletConnect(walletOpt)
+const intiialize = async () => {
+    await wallet.value.reconnectSession()
+}
+const transact = () => {
+    closePeraWalletSignTxnToast()
+}
+const handleDisconnectWalletClick = () => {
+    wallet.value.disconnect();
+    setAccountAddress(null);
+}
+const handleConnectWalletClick = () => {
+    wallet.value
+    .connect()
+    .then((newAccounts) => {
+        setAccountAddress(newAccounts[0]);
+    })
+}
+intiialize()
+</script>
+<template>
+    <div>
+        <p>
+            {{ wallet }}
+        </p>
+    </div>
+
+    <div>
+        <button v-if="!wallet.connector" @click="handleConnectWalletClick">Connect</button>
+        <button v-else @click="handleDisconnectWalletClick">Disconnect</button>
+    </div>
+
+</template>
